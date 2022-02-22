72a8e8acc3c28d35a1cead1fe92dba1e728ad823
aanusemahu <aanusemahu@gmail.com>
2022-02-22 07:17:37 +0700

    button component, logic, hooks, implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -18,7 +19,11 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
 
-    const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch()
+    const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } = useHomeFetch()
+
+    console.log(state)
+
+    if(error) return <div>Something went wrong . . . </div>
 
     return (
         <>
@@ -39,7 +44,10 @@ const Home = () => {
                     />
                 )) }
             </Grid>
-            <Spinner />
+            {loading && <Spinner />}
+            {state.page < state.total_pages && !loading && (
+                <Button text={ 'Load More' } callback={ () => setIsLoadingMore(true) } />
+            )}
         </>
     )
 }
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -15,8 +15,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
-
-    console.log(searchTerm)
+    const [isLoadingMore,setIsLoadingMore] = useState(false)
 
     const fetchMovies = async (page, searchTerm = '') => {
         try {
@@ -42,5 +41,14 @@ export const useHomeFetch = () => {
         fetchMovies(1, searchTerm)
     }, [searchTerm])
 
-    return { state, loading, error, searchTerm, setSearchTerm };
+    // Load More
+    useEffect(() => {
+        if(!isLoadingMore) return;
+
+        fetchMovies(state.page + 1, searchTerm)
+        setIsLoadingMore(false)
+
+    }, [isLoadingMore, searchTerm, state.page])
+
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
