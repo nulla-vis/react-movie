feeb1a6ba18db12eed2cbc8e359eb9aeb45c10a5
aanusemahu <aanusemahu@gmail.com>
2022-02-23 11:52:46 +0700

    Add Actors Component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ import Spinner from "./Spinner";
 import BreadCrumb from "./CreadCrumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfobar from "./MovieInfoBar";
+import Actor from "./Actor";
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch'
 // Image
@@ -26,6 +27,19 @@ const Movie = () => {
             <BreadCrumb movieTitle={ movie.original_title } />
             <MovieInfo movie={ movie } />
             <MovieInfobar time={ movie.runtime } budget={ movie.budget } revenue={ movie.revenue } />
+            <Grid header='Actors'>
+                { movie.actors.map(actor => (
+                    <Actor 
+                        key= { actor.credit_id }
+                        name= { actor.name }
+                        character= { actor.character }
+                        imageUrl= { actor.profile_path
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                            : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
         </>
     )
 }
